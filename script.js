0e525ebf6e49f8e0b6fa85539e10e4b074f2231e
Kevin Guzman <kevincastrog28@gmail.com>
2024-11-10 14:34:08 -0500

    Added javascript to handle calculations

--- script.js
+++ script.js
@@ -1,7 +1,52 @@
 let items = document.querySelectorAll('#item');
+let firstNumInput = document.getElementById('first-num');
+let operatorInput = document.getElementById('operator');
+let secondNumInput = document.getElementById('second-num');
+let resultInput = document.getElementById('result');
 
+//Adds click event listener to items
 items.forEach(item => {
-    item.addEventListener("click", function(numInput) {
-        console.log(numInput.target.value);
+    item.addEventListener('click', function(event) {
+        let value = event.target.value;
+        //clears all values for each variable
+        if(value === 'C') {
+            firstNumInput.value = '';
+            operatorInput.value = '';
+            secondNumInput.value = '';
+            resultInput.value = '';
+        // assigns values to each variable based on value assigned in HTML.
+         } else if(value === '='){
+            let firstNum = parseFloat(firstNumInput.value);
+            let operator = operatorInput.value;
+            let secondNum = parseFloat(secondNumInput.value);
+            let result;
+            // Switch case that handles the math.
+            switch (operator) {
+                case '+':
+                    result = firstNum + secondNum;
+                    break;
+                case '-': 
+                    result = firstNum - secondNum;
+                    break;
+                case '*':
+                    result = firstNum * secondNum;
+                    break;
+                case '/':
+                    result = firstNum / secondNum;
+                    break;
+                default:
+                    result = 'Error!';
+            }
+
+            resultInput.value = result;
+         } else if (['+', '-', '*', '/'].includes(value)) {
+            operatorInput.value = value;
+         } else {
+            if (operatorInput.value === '') {
+                firstNumInput.value += value;
+            } else {
+                secondNumInput.value += value;
+            }
+         }
     });
 });
